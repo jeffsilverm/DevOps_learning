5006096253dbe18c8e072e0fecc7dce37c57116f
Jeffrey Silverman <jsilverman@juniper.net>
2021-01-27 22:09:12 -0800

    From https://pythonspot.com/flask-hello-world/

---
+++ helloWorld/hello.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+#
+# This is from https://pythonspot.com/flask-hello-world/
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run()
