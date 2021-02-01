431585c2b56419df2d3999705fd20c961e2fb08c
Jeffrey Silverman <jsilverman@juniper.net>
2021-02-01 01:12:35 -0800

    Returns my own address but that's not useful

--- helloWorld/hello.py
+++ helloWorld/hello.py
@@ -2,11 +2,27 @@
 #
 # This is from https://pythonspot.com/flask-hello-world/
 from flask import Flask
+import socket
+
+
+# getaddrinfo retrurns a list with 3 elements: one for stream, which is what we
+# want, one for datagrams (UDP), and one for raw sockets.
+af, socktype, proto, canonname, sa = \
+        socket.getaddrinfo(socket.gethostname(), port=80)[0]  # stream = TCP
+assert socktype == socket.SocketKind.SOCK_STREAM, \
+    f"socktype should be socket.SocketKind.SOCK_STREAM but is actually " \
+    f"{str(socktype)}."
+my_address = sa[0]
+
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    global my_address
+    return "Hello World! My address is " + my_address
+
 
 if __name__ == "__main__":
     app.run()
