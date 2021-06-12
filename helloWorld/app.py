bcfbc536ba86325fe5c3dcd1766b77df8deed59a
Jeffrey Silverman <jsilverman@juniper.net>
2021-06-11 23:46:54 -0700

    Lots of changes

--- helloWorld/app.py
+++ helloWorld/app.py
@@ -9,47 +9,6 @@ from flask import request
 app = Flask(__name__)
 pp = pprint.PrettyPrinter(indent=2 )
 
-@app.route('/')
-def hello():
-    return render_template('index.html')
-# curl -i http://localhost:5000
-# curl -i http://localhost:5000/
-
-# The favorite icon is ignored for now
-@app.route('/favicon.ico')
-def favicon():
-    print("In favicon", file=sys.stderr)
-    return "favicon.ico"
-
-@app.route('/<string:page_name>/')
-def render_static(page_name):
-    return render_template('%s.html' % page_name)
-# curl -i http://localhost:5000/index/
-
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return 'User %s\n' % escape(username)
-
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return 'Post %d\n' % post_id
-
-@app.route('/path/<path:subpath>')
-def show_subpath(subpath):
-    # show the subpath after /path/
-    return 'Subpath %s\n' % escape(subpath)
-
-# See https://flask.palletsprojects.com/en/2.0.x/quickstart/#the-request-object
-#with app.test_request_context():
-    # print(url_for('index.html')) # This raises an exception, werkzeug
-    # .routing.BuildError: Could not build url for endpoint
-    # 'index.html'. Did you mean &apos;render_static&apos; instead?
-    # I don't know what meant.
-#    print(url_for('hello'))
-#    print(url_for('login', next='/'))
-#    print(url_for('profile', username='John Doe'))
 
 @app.route('/login/', methods=['POST', 'GET'])
 def login():
@@ -68,7 +27,21 @@ def login():
     username = request.form.get('username')
     password = request.form.get('password')
     app.logger.info(f"{request.environ['REMOTE_ADDR']} , {username}, {password}")
-    return f'I got to the end of method login.  username={username} and password={password}'
+    return f'I got to the end of method login.  username={username} and password={password}\n'
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+# curl -i http://localhost:5000
+# curl -i http://localhost:5000/
+
+# The favorite icon is ignored for now
+@app.route('/favicon.ico')
+def favicon():
+    print("In favicon", file=sys.stderr)
+    return "favicon.ico"
+
+
 
 if __name__ == '__main__':
 # See https://flask.palletsprojects.com/en/2.0.x/debugging/#the-built-in-debugger
